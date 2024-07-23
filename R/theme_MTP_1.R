123a33281290f92192dcd315e8013ddf11fcc29b
Thomson Harris <thomsonharris@gmail.com>
2024-07-23 11:12:33 -0700

    Update to function package

--- R/theme_MTP_1.R
+++ R/theme_MTP_1.R
@@ -1,40 +1,48 @@
 
+#' Theme for MTP graphics
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
 
-theme_MTP <- function() {
+
+theme_MTP_1 <- function() {
   font <- "Times"   #assign font family up front
 
-  theme(
+  ggplot2::theme(
 
     #grid elements
-    panel.background = element_rect(                   # background
+    panel.background = ggplot2::element_rect(                   # background
       fill = "white",                   # background
       color = "black",                  # boarder
       linewidth = 0.75                  # boarder width
     ),
 
-    panel.grid.major = element_line(                 # major grid lines
+    panel.grid.major = ggplot2::element_line(                 # major grid lines
       color = "gray60",                 # line color
       linewidth = 0.05                  # line size
     ),
 
-    panel.grid.minor = element_blank(),    # strip minor grid lines
-    axis.ticks = element_blank(),          # strip axis ticks
+    panel.grid.minor = ggplot2::element_blank(),    # strip minor grid lines
+    axis.ticks = ggplot2::element_blank(),          # strip axis ticks
 
 
     #text elements
-    plot.title = element_blank(),    # remove title
+    plot.title = ggplot2::element_blank(),    # remove title
 
-    axis.title = element_text(             #axis titles
+    axis.title = ggplot2::element_text(             #axis titles
       family = font,            # font family
       face = "bold",            # font style
       size = 18),               # font size
 
-    axis.text = element_text(              #axis text
+    axis.text = ggplot2::element_text(              #axis text
       family = font,             # axis family
       size = 15),                # font size
 
-    axis.text.x = element_text(            #margin for axis text
-      margin = margin(5, b = 10))
+    axis.text.x = ggplot2::element_text(            #margin for axis text
+      margin = ggplot2::margin(5, b = 10))
 
     #since the legend often requires manual tweaking
     #based on plot content, don't define it here
